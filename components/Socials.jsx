7e8b4bcc9b2785f587f60b888df0a4c024458ddc
unknown <jimohabdulmajeed9@gmail.com>
2025-03-16 18:32:10 +0100

    Your commit message

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -3,10 +3,10 @@ import Link from "next/link";
 import { FaGithub, FaLinkedinIn, FaYoutube, FaTwitter } from "react-icons/fa";
 
 const socials =[
-    { icon: <FaGithub />, path: "" },
-    { icon: <FaLinkedinIn />, path: "" },
-    { icon: <FaYoutube  />, path: "" },
-    { icon: <FaTwitter  />, path: "" },
+    { icon: <FaGithub />, path: "https://github.com/jimohabdulmajeed" },
+    { icon: <FaLinkedinIn />, path: "https://www.linkedin.com/in/abdulmajeed-okaka-jimoh-485327261?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" },
+    { icon: <FaYoutube  />, path: "https://youtube.com/@jimohabdulmajeed729?si=eAW1fj4yCZBQwwEH" },
+    { icon: <FaTwitter  />, path: "https://x.com/Abdulmajeed6424?t=91wl02VANe5cEwx-0XH8kQ&s=09" },
   
 ];
 
